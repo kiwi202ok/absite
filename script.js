16905bd8a1cbecf776dceca1ed2ecad7ac1e4083
Maqsudjon <murodovabasira@gmail.com>
2026-02-05 23:23:42 +0500

    fdfd

--- script.js
+++ script.js
@@ -22,60 +22,62 @@ function showInfo(type) {
     document.getElementById("services").classList.add("hidden");
     document.getElementById("info").classList.remove("hidden");
 
-    let data = {
-        beltepa: {
-            wifi: `
-                <b>WiFi nomi:</b> Beltepa_Net<br>
-                <b>Shartnoma egasi:</b>Muradov Mansurjan Nasirovich<br>
-                <b>Login:</b>m615538<br>
-                <b>Hisob raqam(pul tashashga):</b> 1942441724
-            `,
-            gaz: `
-                <b>Gaz hisob raqam:</b> 111222<br>
-                <b>Egasi:</b> Ismingiz
-            `,
-            suv: `
-                <b>Issiq suv:</b> Markaziy<br>
-                <b>Hisob raqam:</b> 555666
-            `,
-            kadaster: `
-                <b>Kadastr raqam:</b> 77:88:999<br>
-                <b>Maydoni:</b> 60 m²
-            `,
-            propiska: `
-                <b>Kadastr raqam:</b><br>
-                <b>Maydoni:</b> 60 m²
-            `
-        },
-        oxunboboyev: {
-            wifi: `
-                <b>WiFi nomi:</b> Oxun_Net<br>
-                <b>Egasi:</b> Ismingiz
+let data = {
+    beltepa: {
+        wifi: `
+            <b>WiFi nomi:</b> Beltepa_Net<br>
+            <b>Shartnoma egasi:</b>Muradov Mansurjan Nasirovich<br>
+            <b>Login:</b>m615538<br>
+            <b>Hisob raqam(pul tashashga):</b> 1942441724
+        `,
+
+        gaz: `
+            <b>Gaz hisob raqam:</b>....<br>
+            <b>Egasi:</b> 
+        `,
+
+        suv: `
+            <b>Issiq suv:</b>...<br>
+            <b>Hisob raqam:</b> ....
+        `,
+        kadaster: `
+            <b>Kadastr raqam:</b> 77:88:999<br>
+            <b>Maydoni:</b> 60 m²
+        `,
+        propiska: `
+            <b>Kadastr raqam:</b><br>
+            <b>Maydoni:</b> 60 m²
+         `
+    },
+    oxunboboyev: {
+        wifi: `
+            <b>WiFi nomi:</b> Oxun_Net<br>
+            <b>Egasi:</b> Ismingiz
             `,
 
-            gaz: `
-                <b>Gaz hisob raqam:  </b> 1008018628
+        gaz: `
+            <b>Gaz hisob raqam:  </b> 1008018628
             `,
 
-            suv: `
-                <b>Issiq suv:</b>
+        suvissiq: `
+            <b>Issiq suv:</b>
             `,
 
-            suv: `
-                <b>Sovuq suv:  </b>2628050464
+        suvsocuq: `
+            <b>Sovuq suv:  </b>2628050464
             `,
             
-            kadaster: `
-                <b>Kadastr:  </b>10:08:01:01:01:5405
-            `,
-            propiska: `
-                <b>Propiska:  </b>10:08:01:01:01:5405<br>
-                <b>Maydoni:</b> 262.19 m²
-            `,
-            musor: `
-                <b>Musor:  </b>10:08:01:01:01:5405<br>
-                <b>Maydoni:</b> 262.19 m²
-            `
+        kadaster: `
+            <b>Kadastr:  </b>10:08:01:01:01:5405
+             `,
+        propiska: `
+            <b>Propiska:  </b>10:08:01:01:01:5405<br>
+            <b>Maydoni:</b> 262.19 m²
+             `,
+         musor: `
+            <b>Musor:  </b>10:08:01:01:01:5405<br>
+            <b>Maydoni:</b> 262.19 m²
+             `
         }
     };
 
